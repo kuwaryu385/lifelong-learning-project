11cbe90df72a60ba14b4ac6c26c6b8ed45dc97e9
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-10-28 21:26:48 +0900

    スッキリわかる実践学習　文字パターンとStringBuilderについて

--- LifelongLearning/src/practicesukiri/Main.java
+++ LifelongLearning/src/practicesukiri/Main.java
@@ -3,8 +3,28 @@ package practicesukiri;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
 	}
-
 }
+
+//文字列パターンを用いたペット名のチェック
+//Stringクラスのmatches()メソッドの使用
+//matches(最初の文字に数字はつかえない,使える数字はA~Zと０〜９だけ、８文字の指定）の文字列パターン
+//	boolean isValidPetName(String name) {
+//		return name.matches("[A-Z] [A-Z0-9]{7}");//文字列パターン
+//	}
+
+//StringBuilderを利用し文字列を１万回無駄ラッシュを表示する
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		StringBuilder sb = new StringBuilder();
+//		for (int i = 0; i < 10000; i++) {
+//			sb.append("無駄");
+//		}
+//		String s = sb.toString();
+//		System.out.println(s);
+//
+//	}
+//
+//}
