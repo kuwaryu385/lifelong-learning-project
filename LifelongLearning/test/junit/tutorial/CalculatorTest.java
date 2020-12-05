4b1cb8c5063810e10f284fd313ba655754397fcd
Ryuta Kuwamura <staticjbulejass@gmail.com>
2020-12-05 10:33:11 +0900

    チュートリアルでテストコードの追加

--- LifelongLearning/test/junit/tutorial/CalculatorTest.java
+++ LifelongLearning/test/junit/tutorial/CalculatorTest.java
@@ -1,14 +1,20 @@
 package junit.tutorial;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junittutorial.Calculator;
+
 public class CalculatorTest {
 
 	@Test
-	public void test() {
-		fail("まだ実装されていません");
+	public void multiplyで乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
 	}
 
 }
